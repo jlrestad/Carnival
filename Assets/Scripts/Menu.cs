d642c092b20fcbe7860c2cb4784a0e6ab66b6e51
jlrestad <jl.restad@gmail.com>
2021-07-31 02:12:16 -0700

    Fixed scene order.

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -39,7 +39,7 @@ public class Menu : MonoBehaviour
     {
         // Start game scene
         GM.SetGameState(GameState.LEVEL_ONE);
-        SceneManager.LoadScene("Level01", LoadSceneMode.Single);
+        SceneManager.LoadScene("SandBox", LoadSceneMode.Single);
         Debug.Log(GM.gameState);
     }
 
