2ae87c2d619a97dee8c06ec5960a603e606969ad
Christian Hilson <S-Christian.Hilson@lwtech.edu>
2022-05-03 13:04:20 -0700

    minor edits to accommodate end of the fight

--- Assets/Scripts/BossBehavior.cs
+++ Assets/Scripts/BossBehavior.cs
@@ -12,8 +12,9 @@ public class BossBehavior : MonoBehaviour
     [SerializeField] float turnSpeed; //turn speed in degrees per second
     [SerializeField] private float timeCounter = 0; //used separate time between taunt / leftswing / rightswing
 
-    private int whichHit = 0;               // needs to increment everytime the heart is hit
-    private int patternNumber = 0;
+    int whichHit = 0;               // needs to increment everytime the heart is hit
+    int patternNumber = 0;
+    bool bossAlive = true;
 
     private void Awake()
     {
@@ -24,28 +25,40 @@ public class BossBehavior : MonoBehaviour
 
     private void Update()
     {
-        if(timeCounter == 0)     // will make boss start off with a taunt and then follow the attack pattern for rest of fight
+        if(bossAlive)
         {
-            StartCoroutine(AttackPattern());
+            if (timeCounter == 0)     // will make boss start off with a taunt and then follow the attack pattern for rest of fight
+            {
+                StartCoroutine(AttackPattern());
 
-            RandomizeTimer(1, 4 - whichHit); // randomizes swing / taunt time depending on how many times the heart has been hit
+                RandomizeTimer(1, 4 - whichHit); // randomizes swing / taunt time depending on how many times the heart has been hit
+            }
+
+            StartCoroutine(CountDownTimer());
+            //Calculate distance to the player
+            distanceFromPlayer = transform.position - player.transform.position;
+
+
+            if (distanceFromPlayer.magnitude <= maxDistance)
+            {
+                //Chase player
+                agent.destination = (player.transform.position);
+
+                //Look at player
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, player.transform.rotation, turnSpeed * Time.deltaTime);
+
+            }
+
+            if(whichHit == 3)
+            {
+                bossAlive = false;
+            }
         }
 
-        StartCoroutine(CountDownTimer());
-        //Calculate distance to the player
-        distanceFromPlayer = transform.position - player.transform.position;
-
-
-        if (distanceFromPlayer.magnitude <= maxDistance)
+        if(!bossAlive)
         {
-            //Chase player
-            agent.destination = (player.transform.position);
-
-            //Look at player
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, player.transform.rotation, turnSpeed * Time.deltaTime);
-
+            Death();
         }
-
         //* Create a field of vision. If player is within field of vision, then chase player.
         //* if player is within x distance -- attack
         //* if player cannot be found, then search for player -- look around, move around, until player is detected
@@ -67,6 +80,11 @@ public class BossBehavior : MonoBehaviour
     private void RightSwing()
     {
         // need left swing animation to happen when called
+    }
+
+    private void Death()
+    {
+        // need death animation
     }
     IEnumerator AttackPattern()
     {
