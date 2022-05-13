ab307b7e36e4fa8bc0e3e490e9b6aa59b48ec6da
Kristina Postor <kpostor@hotmail.com>
2022-05-13 03:45:20 -0700

    detect collisions with critters during boss fight
    modified critter prefab (is trigger now). Still needs to be hooked up to red tickets

--- Assets/Scripts/WhackEmEnemy.cs
+++ Assets/Scripts/WhackEmEnemy.cs
@@ -33,4 +33,16 @@ public class WhackEmEnemy : MonoBehaviour
         }
     }
 
+
+    // For boss fight. Turned prefabs into triggers to allow
+    // For some reason OnCollisionEnter does not work.
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("COLLISON using trigger");
+            // need to hook up red cards for player health during boss fight
+        }
+    }
+
 }
