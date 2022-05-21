3f3997d566c93a9ad3524de42a2f10252e6ef368
Kristina Postor <kpostor@hotmail.com>
2022-05-21 15:18:19 -0700

    fixed boss critters rotation
    only rotates on y axis now

--- Assets/Scripts/BossCritterBehaviors.cs
+++ Assets/Scripts/BossCritterBehaviors.cs
@@ -7,13 +7,20 @@ public class BossCritterBehaviors : MonoBehaviour
 
     public GameObject player;
     public Transform target;
+    public Quaternion initial;
 
     private void Update()
     {
+       
         player = GameObject.FindGameObjectWithTag("Player");
         target = player.transform;
 
-        transform.LookAt(target);
+        Vector3 dir = target.position - transform.position;
+        Quaternion rotation = Quaternion.LookRotation(dir);
+
+        Vector3 newRot = Quaternion.Lerp(this.transform.rotation, rotation, 1).eulerAngles;
+        this.transform.rotation = Quaternion.Euler(0f, newRot.y, 0f);
+
     }
 
     // For boss fight. Turned prefabs into triggers to allow
