7e9e5a59fa8326fdb8f7abf2780d26f0e46ec96b
jlrestad <jl.restad@gmail.com>
2022-06-14 03:32:49 -0700

    Fixed Win/Lose text popping up at the start of the game. Fixed tarot card

---
+++ Assets/Scripts/SelectedCard.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectedCard : MonoBehaviour
+{
+    //Used to find the game card to highlight which weapon is equipped.
+}
